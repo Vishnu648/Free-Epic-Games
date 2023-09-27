12024d7ac92740dc840d1813c9789ae0e4821b0d
Vishnu V S <96856622+Vishnu648@users.noreply.github.com>
2023-09-27 07:30:42 +0530

    Update useFetch.jsx

--- src/utils/useFetch.jsx
+++ src/utils/useFetch.jsx
@@ -6,12 +6,13 @@ import axios from 'axios'
 const useFetch = (endpoint) => {
 
     const [games, setGames] = useState([])
-
+    const key=d99907482cmsh9ab0fc66bc0daf3p153337jsn95f51e31894e;
+    
     const options = {
         method: 'GET',
         url: `https://epic-free-games.p.rapidapi.com/${endpoint}`,
         headers: {
-            'X-RapidAPI-Key':d99907482cmsh9ab0fc66bc0daf3p153337jsn95f51e31894e,
+            'X-RapidAPI-Key': key,
             'X-RapidAPI-Host': 'epic-free-games.p.rapidapi.com'
         }
     };
